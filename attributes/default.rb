57882e3d8bd686b6f63d9ac0d5bfbea6fe95bce8
anant jain <anant.jain@optimusinfo.vom>
2013-05-17 13:28:09 +0530

    firefox in path and disable auto update

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default["firefox"]["user"] = "ubuntu"
+default["firefox"]["group"] = "ubuntu"
+default["firefox"]["download_url"] = "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/15.0b6/linux-x86_64/en-US/firefox-15.0b6.tar.bz2"
+default["firefox"]["source_file"] = "firefox-15.0b6.tar.bz2"
